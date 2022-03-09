d05c5043de772e75c3fb98a1c873f526a4166fce
its1sttimeee <jakkaf1rst@gmail.com>
2022-03-09 22:13:40 +0700

    Final EX4

---
+++ Exercise4_Jakkapong_Y.py
@@ -0,0 +1,16 @@
+fe = "Foundation English :"
+fe_score = 70
+gb = "General Business :"
+gb_score = 69
+ics = "Introduction to Computer Systems :"
+ics_score = 77.5
+cp = "Computer Programming :"
+cp_score = 88
+print("--- your score ---")
+print(fe, fe_score)
+print(gb, gb_score)
+print(ics, ics_score)
+print(cp, cp_score)
+
+
+
