f6a4b1b9758323ac51315fa55f2a5ba64c2df546
its1sttimeee <jakkaf1rst@gmail.com>
2022-03-14 22:37:51 +0700

    Final Ex9

---
+++ Exercise9_Jakkapong_Y.py
@@ -0,0 +1,7 @@
+username = input("Username : ")
+password = input("Password : ")
+while username != "xyc123" or password != "pass123" :
+    print("You username or password incorect.")
+    username = input("Username : ")
+    password = input("Password : ")
+print("Done!!")
