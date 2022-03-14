9c4c745494c9ed6789df18b6397fd8feaac8b664
its1sttimeee <jakkaf1rst@gmail.com>
2022-03-14 15:36:23 +0700

    Final Ex5_1

---
+++ Exercise5_1_Jakkapong_y.py
@@ -0,0 +1,6 @@
+num1 = (input("Put 1 number : "))
+num2 = (input("Put one more number : "))
+print(num1,"+", num2, "=", int(num1) + int(num2))
+print(num1,"-", num2, "=", int(num1) - int(num2))
+print(num1,"*", num2, "=", int(num1)*int(num2))
+print(num1,"/", num2, "=", int(num1)/int(num2))
