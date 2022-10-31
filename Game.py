b026bad0309f62540494733f48f8b5f3fd1d6e23
its1sttimeee <jakkaf1rst@gmail.com>
2022-10-31 21:10:39 +0700

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,115 @@
+import pygame
+import random
+import sys
+
+pygame.init()
+
+#กำหนดสี
+p_color = (240,255,255)
+e_color = (220,20,60)
+BG_Cl = (0,0,0)
+
+#กำหนดขนาดเกม
+W = 750
+H = 550
+
+
+
+p_size = 50
+p_pos = [400, 500]
+
+e_size = 30
+e_pos = [random.randint(0,775), 0]
+e_list =[e_pos]
+
+
+
+show = pygame.display.set_mode((W, H))
+
+Lose = False
+
+score = 0
+
+clock = pygame.time.Clock()
+
+
+
+SPEED = 15
+
+#สร้างอุปสรรคโดยการสุ่มเกิด
+def drop_enemies(e_list):
+	delay = random.random()
+	if len(e_list) < 10 and delay < 0.1:
+		x_pos = random.randint(0,775)
+		y_pos = 0
+		e_list.append([x_pos, y_pos])
+
+#ฟังก์ชันทำให้เห็นอุปสรรค
+def appear_enemies(e_list):
+	for e_pos in e_list:
+		pygame.draw.rect(show, e_color, (e_pos[0], e_pos[1], e_size, e_size))
+
+def enemy_pos(e_list, score):
+	for idx, e_pos in enumerate(e_list):
+		if e_pos[1] >= 0 and e_pos[1] < H:
+			e_pos[1] += SPEED
+		else:
+			e_list.pop(idx)
+			score += 1
+	return score
+
+def collision_check(e_list, p_pos):
+	for e_pos in e_list:
+		if detect_collision(e_pos, p_pos):
+			return True
+	return False
+
+def detect_collision(p_pos,e_pos):
+	player_x = p_pos[0]
+	player_y = p_pos[1]
+
+	e_x = e_pos[0]
+	e_y = e_pos[1]
+
+	if (e_x >= player_x and e_x < (player_x + p_size)) or (player_x >= e_x and player_x < (e_x + e_size)):
+		if (e_y >= player_y and e_y < (player_y + p_size)) or (player_y >= e_y and player_y < (e_y + e_size)):
+			return  True
+	return False
+
+while not Lose:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+		if event.type == pygame.KEYDOWN:
+
+			x = p_pos[0]
+			y = p_pos[1]
+
+			if event.key == pygame.K_LEFT:
+				x -= p_size
+			elif event.key == pygame.K_RIGHT:
+				x += p_size
+
+			p_pos = [x,y]
+
+	show.fill(BG_Cl)
+
+	drop_enemies(e_list)
+	score = enemy_pos(e_list, score)
+
+
+
+
+	if collision_check(e_list, p_pos):
+		game_over = True
+		break
+
+	appear_enemies(e_list)
+	clock.tick(40)
+	pygame.draw.rect(show, p_color, (p_pos[0], p_pos[1], p_size, p_size))
+
+
+
+	pygame.display.update()
